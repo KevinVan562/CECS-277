07d9ba5e844ecc2210e5cc12febf6fd47944eb17
Kevin Van <itiskevinvan@gmail.com>
2024-01-28 16:16:59 -0800

    autopep8 for checkinput

--- Review/main.py
+++ Review/main.py
@@ -1,8 +1,10 @@
 import check_input
 
-def main():
-  val = check_input.get_int_range("Enter a number 1-10: ", 1, 10)
 
-  print("Val = " + str(val))
+def main():
+    val = check_input.get_int_range("Enter a number 1-10: ", 1, 10)
+
+    print("Val = " + str(val))
+
 
 main()
