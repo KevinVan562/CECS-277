7eecb68a35ba7c1906623bb86db3cf8a268fe70f
Kevin Van <itiskevinvan@gmail.com>
2024-02-16 01:25:33 -0800

    Finished code

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,4 +1,8 @@
-import check_input
+# Name: Kevin Van & Isai Beltran
+# Date: 02/12/2024
+# Description: 10 question state capital quiz game. Depending on the user's correct
+# answers, they will be given a score out of 10.
+
 import random
 
 
@@ -6,21 +10,21 @@ def read_file(file_name):
     """Reads the file and returns the list of words"""
     with open(file_name) as file:
         result_list = []
-        for state in file:
-            split_list = state.strip().split(',')
+        for row in file:
+            split_list = row.strip().split(',')
             result_list.append(split_list)
         return result_list
 
 
 def get_random_state(states):
     """This function will return a random state from the list of states"""
-    random_num = random.randint(0, 49)
-    random_state = states[random_num]
+    random_number = random.randint(0, 49)
+    random_state = states[random_number]
     return random_state
 
 
 def get_random_choices(states, correct_capital):
-    """Pass in the list of states and the correct answer. Randomly assigns
+    """Passes in the list of states and the correct answer. Randomly assigns
     three incorrect choices to a list and returns that list."""
     wrong_list = []
     count = 0
@@ -37,38 +41,58 @@ def get_random_choices(states, correct_capital):
 
 
 def ask_question(correct_state, possible_answers):
-    counter = 1
+    """Takes user's selection and checks if it is A,B,C,D if it isn't display
+    an error messageand ask the question again"""
     while True:
-        user_selection = input(
-            f"{counter}. The Capital of {correct_state} is: \n"
-            f"a) {possible_answers[0][0]} \n"
-            f"b) {possible_answers[1][0]} \n"
-            f"c) {possible_answers[2][0]} \n"
-            f"d) {possible_answers[3][0]} \n")
-    if user_selection == 'A':
-        counter += 1
-        return 0
-    elif user_selection == 'B':
-        counter += 1
-        return 1
-    elif user_selection == 'C':
-        counter += 1
-        return 2
-    elif user_selection == 'D':
-        counter += 1
-        return 3
-    else:
-        print("Invalid Entry. Please input valid entry")
+        user_input = input("\nEnter Selection: ").upper()
+
+        if user_input == 'A':
+            return 0
+        elif user_input == 'B':
+            return 1
+        elif user_input == 'C':
+            return 2
+        elif user_input == 'D':
+            return 3
+        else:
+            # To not cause an unnecessary newline
+            print("Invalid Input. Input choice A-D.", end="")
+        pass
 
 
 def main():
-    data_list = read_file("statecapitals.txt")
-    random_state = get_random_state(data_list)
-    choices = get_random_choices(data_list, random_state)
-    print("- State Capital Quiz -")
-    correct_guesses = 0
-    count = 0
 
-    ask_question(random_state[1], choices)
+    data_list = read_file("statecapitals.txt")
+    total_points = 0
+    quiz_question = 1
+
+    print("- State Capitals Quiz -")
+
+    while quiz_question < 11:
+        """This while loop will run 10 times and will ask the user a question"""
+        correct_answer = get_random_state(data_list)
+        choices = get_random_choices(data_list, correct_answer)
+
+        print(f"{quiz_question}. The capital of {correct_answer[0]} is: ")
+        # Indents in prints for readability in output
+        print(f"     A. {choices[0][1]}", end=" ")
+        print(f"  B. {choices[1][1]}", end=" ")
+        print(f"  C. {choices[2][1]}", end=" ")
+        print(f"  D. {choices[3][1]}", end=" ")
+
+        user_selection = ask_question(
+            correct_answer, choices)  # Gets user input
+
+        if choices[user_selection][1] == correct_answer[1]:
+            """If the user's selection is correct, add 1 to the total points"""
+            print("Correct!")
+            total_points += 1
+        else:
+            print("Incorrect! The correct answer is:", correct_answer[1])
+
+        quiz_question += 1  # increments the question number
+
+    print("End of test. You got", total_points, "correct.")
+
 
 main()
